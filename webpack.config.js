d74cfd2839345076cbf7fcecaa7cee1385378616
Denis Stebunov <denis.stebunov@ivelum.com>
2021-05-18 14:33:29 +0300

    introducing basic webpack build

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'completion.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  ie: "9",
+                },
+              }
+            ]
+          ],
+        }
+      }
+    ]
+  },
+};
